793f9834533193d1b0dbcac7e45e1ae8be4facdc
ktuttle92 <ktuttle92@gmail.com>
2025-12-30 13:11:56 -0500

    second commit

---
+++ module4.js
@@ -0,0 +1,13 @@
+//template_fyrzg4l
+//service_6ie651b
+//S37FKqKjXavUxX
+
+function contact(event){
+    event.preventDefault();
+    emailjs .sendForm{
+        'service_6ie651b',
+        'template_fyrzg4l',
+        event.target
+
+    }
+}
