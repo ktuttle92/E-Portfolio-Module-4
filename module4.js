7e48cbdb8001adb87577c2f45cbd44d1f2883464
ktuttle92 <ktuttle92@gmail.com>
2025-12-31 13:11:29 -0500

    third commit

--- module4.js
+++ module4.js
@@ -4,10 +4,29 @@
 
 function contact(event){
     event.preventDefault();
-    emailjs .sendForm{
+        const loading = document.querySelector('.modal__overlay--loading')
+        const success = document.querySelector('.modal__overlay--success')
+        loading.classList += " modal__overlay--visible"
+    emailjs 
+     .sendForm(
         'service_6ie651b',
         'template_fyrzg4l',
-        event.target
+        event.target,
+        "6RQS37FKqKjXavUxX"
+     ).then(() =>{
+            loading.classList.remove("modal__overlay--visible");
+            success.classList += " modal__overlay--visible";
+     }).catch(() => {
+            loading.classList.remove("modal__overlay--visible");
+            alert("The email service is temporarily unavailable. Please contact me directly at ktuttle92@gmail.com")})
+}
 
+let isModalOpen = false;
+function toggleModal(){
+    if(isModalOpen){
+        isModalOpen=false
+        return document.body.classList.remove("modal--open")
     }
+    isModalOpen = true
+   document.body.classList += " modal--open"
 }
